b0914e4260233a15b861edf4f6d5cb2a630ce383
Aritree Hoque <aritree.hoque@gmail.com>
2025-03-02 23:14:45 -0500

    Updated innerHTML for task1

--- cc_12.js
+++ cc_12.js
@@ -1,14 +1,11 @@
 // Task1- Business Dashboard- DOM Element Selection and Creation
-const dashboardId = document.getElementById("dashbaord");
+const dashboard = document.getElementById("dashbaord");
 const dashboardQuery = document.querySelector("#dashboard");
 const revenueCard = document.createElement("div");
 revenueCard.setAttribute("class", "metric-card");
 revenueCard.setAttribute("id", "revenue-card");
-revenueCard.innerHTML = `
-<h3>Revenue</h3>
-<p>$0</p>
-`;
-dashboardId.appendChild(revenueCard);
+revenueCard.innerHTML = `<h3>Revenue</h3><p>$0</p`;
+dashboard.appendChild(revenueCard);
 
 // Task2- Updating Dashboard Metrivs- Working with Ndelists and Arrays.
 const metricCards = document.querySelectorAll(".metric-card");
